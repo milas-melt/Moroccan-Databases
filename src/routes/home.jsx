ecec66168f0ab91811d78eaa169d0abb5bd8f22a
milas-melt <tlemcani.salim00@gmail.com>
2023-02-25 17:04:21 +0000

    bug fix -recaptcha

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -186,17 +186,7 @@ export default function Home() {
                             >
                                 Get started
                             </Link>
-                            <button
-                                type="button"
-                                className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
-                                onClick={() =>
-                                    console.log(
-                                        process.env.REACT_APP_RECAPTCHA_KEY
-                                    )
-                                }
-                            >
-                                test recaptcha key
-                            </button>
+
                             <Link
                                 to="#"
                                 className="text-sm font-semibold leading-6 text-white"
